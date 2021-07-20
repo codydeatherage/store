b8f3053465be98cf646a9db6463dad9d190ef8e9
codydeatherage <cdeatherage14@gmail.com>
2021-07-19 23:59:09 -0700

    smol components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import NavBar from './NavBar'
+//import { PlayerData, MoviesInsert, MoviesUpdate, SummonerSearch } from '../pages/index'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Switch>
+              {/* <Route path="/" exact component={App}></Route> */}
+                {/* <Route path="/movies/list" exact component={MoviesList} /> */}
+                {/* <Route path="/movies/create" exact component={MoviesInsert} />
+                <Route
+                    path="/movies/update/:id"
+                    exact
+                    component={MoviesUpdate}
+                />
+                <Route path="/search" exact component={SummonerSearch} />
+                <Route path={'/search/:name'} exact component={PlayerData} /> */}
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
